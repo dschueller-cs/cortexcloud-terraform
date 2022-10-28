5836f3905103d85d995e356d0d201112b58fe494
dschueller-cs <dschueller@cognitivescale.com>
2022-10-28 09:41:50 -0400

    wip

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ terraform {
   }
 }
 
-variable "ACCESS_KEY" {
-  type = string
-}
+# variable "ACCESS_KEY" {
+#   type = string
+# }
 
-variable "SECRET_KEY" {
-  type = string
-}
+# variable "SECRET_KEY" {
+#   type = string
+# }
 
 # variable "SESSION_TOKEN" {
 #   type = string
@@ -23,26 +23,16 @@ provider "aws" {
   # Configuration options
   region = "us-east-2"
 
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
+  # access_key = var.ACCESS_KEY
+  # secret_key = var.SECRET_KEY
   # token = var.SESSION_TOKEN
+  profile = "dsdev1-admin"
 
   # Make it faster by skipping some things
   skip_get_ec2_platforms      = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
+  skip_requesting_account_id  = false
 }
 
-module "testbucket1" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "c12etestbucket1"
-  acl = "private"
-}
-
-module "testbucket2" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "c12etestbucket2"
-  acl = "private"
-}
